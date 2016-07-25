da602e042d666b74eecf969efb42dd23ba94c774
Priya <priya.pancholi@promactinfo.com>
2016-07-25 11:32:47 +0530

    owin authentication

---
+++ OwinFollow/Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OwinFollow.Models
+{
+    public class Address
+    {
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public long Pincode { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string ContactNumber { get; set; }
+    }
+}
